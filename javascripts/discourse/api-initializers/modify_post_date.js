af328caf719d878891811278af1615e9f1bd715f
Faizaan Gagan <faizaan@discourse.org>
2023-08-16 14:05:32 +0530

    initial commit

---
+++ javascripts/discourse/api-initializers/modify_post_date.js
@@ -0,0 +1,14 @@
+import { apiInitializer } from "discourse/lib/api";
+import { longDate } from "discourse/lib/formatter";
+
+export default apiInitializer("0.11.1", api => {
+  api.reopenWidget("post-date", {
+    html(attrs) {
+      let test =  this._super(attrs);
+      let date = attrs.wiki && attrs.lastWikiEdit ? new Date(attrs.lastWikiEdit) : new Date(attrs.created_at);
+      console.log(object);
+      test.attrs.rawLabel = longDate(date);
+      return test;
+    }
+  });
+});
